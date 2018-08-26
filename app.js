aac5725b34d7d9b2ae44ac63f5cd58fa710634a6
Maxime Achouche <maxime.achouche-extern@renault.com>
2018-08-26 16:24:17 +0200

    adding some lodash code

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const http = require("http");
+const _ = require("lodash");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const person = {
+    age: 31,
+    name: "Maxime"
+  };
+
+  if (_.isNil(person)) {
+    console.log("coucou");
+  }
+  
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello super World\n");
